208fc7ff4ba32f4690dc3ba757d8440bb18d5ab9
Ali Taş <alitas7875@gmail.com>
2025-04-02 08:21:29 +0300

    Update createCombinationCommand.ts

--- src/services/CombinationServices/Commands/createCombinationCommand.ts
+++ src/services/CombinationServices/Commands/createCombinationCommand.ts
@@ -1,26 +1,26 @@
 import { database } from '../../../config/database';
 import { ref, set } from 'firebase/database';
-import { CustomerProfileDto } from '../../../dtos/CustomerProfile/customerProfileDto';
 import { v4 as uuidv4 } from 'uuid';
+import { IngredientRequestDto } from '../../../dtos/Ingredients/ingredient-request.dto';
 
 interface CreatedCombinationResponse {
     success: boolean;
     data?: {
         id: string;
-        customerProfileData: CustomerProfileDto;
+        ingredientRequest: IngredientRequestDto;
     };
     errorMessage?: string;
 }
 
 export class CreateCombinationCommand {
-    async execute(request: CustomerProfileDto): Promise<CreatedCombinationResponse> {
+    async execute(request: IngredientRequestDto): Promise<CreatedCombinationResponse> {
         try {
             const id = uuidv4();
             const combinationRef = ref(database, `combinations/${id}`);
             
             await set(combinationRef, {
                 id,
-                customerProfileData: request,
+                ingredients: request,
                 createdAt: new Date().toISOString()
             });
             
@@ -28,7 +28,7 @@ export class CreateCombinationCommand {
                 success: true,
                 data: {
                     id,
-                    customerProfileData: request
+                    ingredientRequest: request
                 },
                 errorMessage: 'Combination successfully created'
             };
