b80199f39d661c62d557259ae5a0dd20b63b37c9
Ali Taş <alitas7875@gmail.com>
2025-04-11 07:55:58 +0300

    .

--- src/routes/currentPromptRoutes.ts
+++ src/routes/currentPromptRoutes.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-import { verifyRefreshTokenMiddleware } from '../middleware/authMiddleware';
 import { CurrentPromptController } from '../controllers/currentPromptController';
 
 const router = express.Router();
 const currentPromptController = new CurrentPromptController();
 
-router.post('/currentPrompt/get', verifyRefreshTokenMiddleware, (req, res) => currentPromptController.getCurrentPrompt(req, res));
+router.post('/currentPrompt/getCurrentPrompt', (req, res) => currentPromptController.getCurrentPrompt(req, res));
 
 export default router;
